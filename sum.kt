9e1e9432b381e079dac385c962c396e63d15421d
Ezekiel Holmes <90879385+BSU-EzekielHolmes@users.noreply.github.com>
2022-04-06 16:22:19 -0600

    Corrected a bug in sum.kt

--- sum.kt
+++ sum.kt
@@ -7,7 +7,7 @@ fun main(args: Array<String>) {
     val nums = arrayOf<Int>(1,2,3,4,5)
     var sum = 0
 
-    for(i in 0..nums.size()-1) {
+    for(i in 0..nums.size-1) {
         sum+=nums[i]  
     }
 
