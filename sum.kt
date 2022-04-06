31a89e4ebe5213a13562426eeff26ff6864b97ef
Ezekiel Holmes <90879385+BSU-EzekielHolmes@users.noreply.github.com>
2022-04-06 16:12:16 -0600

    Added sum.kt

---
+++ sum.kt
@@ -0,0 +1,15 @@
+/* Name: Ezekiel Holmes
+*  Description: A simple summation program written in kotlin.
+*  Prints "15" to the console on a new line.
+*/
+
+fun main(args: Array<String>) {
+    val nums = arrayOf<Int>(1,2,3,4,5)
+    var sum = 0
+
+    for(i in 0..nums.size()-1) {
+        sum+=nums[i]  
+    }
+
+    println("$sum")
+}
